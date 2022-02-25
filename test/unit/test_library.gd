2c161fd47769e20b0a1e182f9059da2d354a92d4
Forgotten <forgottenrunes@protonmail.com>
2022-02-25 15:23:00 -0600

    init
    added readme

---
+++ test/unit/test_library.gd
@@ -0,0 +1,13 @@
+extends "res://addons/gut/test.gd"
+
+var Library = load('res://src/library.gd')
+var lib = null
+
+func before_each():
+	lib = Library.new()
+
+func test_assert_eq_number_equal():
+	assert_eq('asdf', 'asdf', "Should pass")
+
+func test_assert_eq_echo():
+	assert_eq("hi mom", lib.echo("hi mom"), "Should echo")
